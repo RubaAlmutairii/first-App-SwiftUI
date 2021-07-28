8288df29bc4e15977f69310b723d7a615d4cb004
Ruba <RubaAlmutairi@gmail.com>
2021-07-28 11:49:28 +0300

    Initial Commit

---
+++ first App SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  first App SwiftUI
+//
+//  Created by Ruba Almutairi on 18/12/1442 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
